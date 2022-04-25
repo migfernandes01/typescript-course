62b7cdb01f2d0240e6506262f1fba581afa8f2c2
Miguel Fernandes <migamado01@gmail.com>
2022-04-25 11:53:27 -0400

    Interface for optimization of decorator

--- ts-express/src/controllers/decorators/routes.ts
+++ ts-express/src/controllers/decorators/routes.ts
@@ -1,12 +1,19 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { RequestHandler } from 'express';
+
+// new interface to make sure the descriptor is applied in a method that has
+// an optional property descriptor value of RequestHandler
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+    value?: RequestHandler;
+}
 
 // function to bind methods to each decorator function
 function routeBinder(method: string) {
     // factory decorator to associate path of GET using metadata 
     return function (path: string) {
-        return function(target: any, key: string, desc: PropertyDescriptor) {
+        return function(target: any, key: string, desc: RouteHandlerDescriptor) {
             // define a 'path' metadata with value of path arg, on the method 'key' of class 'target'
             Reflect.defineMetadata(MetadataKeys.path, path, target, key);
             // define a 'method' metadata with value of method arg, on the method 'key' of class 'target'
